44e91f040cd89bef91b74be632ce132490cdb6c6
Yuzi0201 <2205816944@qq.com>
2022-12-18 21:47:59 +0800

    Site updated: 2022-12-18 21:47:59

--- js/particlex.js
+++ js/particlex.js
@@ -1,69 +1,57 @@
-hljs.highlightAll();
-hljs.configure({ ignoreUnescapedHTML: true });
-var codes = document.getElementsByTagName("pre");
-for (var i = 0; i < codes.length; i++) {
-    var lang = codes[i].firstChild.className.split(/\s+/).filter((x) => {
-        return x != "sourceCode";
-    })[0];
-    if (!lang) lang = "text";
-    codes[i].innerHTML += '<div class="language">' + lang + "</div>";
-}
-const App = Vue.createApp({
+const app = Vue.createApp({
     data() {
         return {
-            show_page: false,
-            menu_show: false,
-            card_top: 100,
-            bar_local: 0,
+            showpage: false,
+            menushow: false,
+            cardtop: 100,
+            barlocal: 0,
         };
     },
     created() {
-        var that = this;
-        window.onload = function () {
-            that.show_page = true;
+        let that = this;
+        window.addEventListener("load", () => {
+            that.showpage = true;
             document.getElementById("loading").style.opacity = 0;
-            setTimeout(function () {
-                document.getElementById("loading").style.display = "none";
-            }, 300);
-        };
+            document.getElementById("loading").style.visibility = "hidden";
+        });
     },
     mounted() {
         if (document.getElementById("home-head"))
             document.getElementById("menu").className += " menu-color";
-        window.addEventListener("scroll", this.handleScroll, true);
-        for (let index = 0; index < codes.length; index++) {
-            const codeblock = codes[index];
-            const copyCode = document.createElement("span");
-            copyCode.className = "copyCode"
-            copyCode.innerText = "复制代码";
-            copyCode.addEventListener('click', async function () {
-                await navigator.clipboard.writeText(this.parentElement.firstChild.innerText)
-                copyCode.innerText = "复制成功！！"
-                setTimeout(() => { copyCode.innerText = "复制代码" }, 3000)
-            })
-            codeblock.appendChild(copyCode);
-        }
+        window.addEventListener("scroll", this.handlescroll, true);
+        highlight();
+        showimg();
+        rendermath();
     },
     methods: {
-        home_click() {
+        homeclick() {
             window.scrollTo({
                 top: window.innerHeight,
                 behavior: "smooth",
             });
         },
-        handleScroll() {
-            var new_local = document.documentElement.scrollTop;
-            var menu = document.getElementById("menu");
-            var that = this;
-            if (this.bar_local < new_local) {
+        handlescroll() {
+            let newlocal = document.documentElement.scrollTop;
+            let menu = document.getElementById("menu");
+            let wrap = document.getElementById("home-posts-wrap");
+            let footer = document.getElementById("footer");
+            let that = this;
+            if (this.barlocal < newlocal) {
                 menu.className = "hidden-menu";
-                that.menu_show = false;
+                that.menushow = false;
             } else menu.className = "show-menu";
-            if (document.getElementById("home-posts-wrap"))
-                if (new_local <= window.innerHeight - 100)
-                    menu.className += " menu-color";
-            this.bar_local = new_local;
+            if (wrap) {
+                if (newlocal <= window.innerHeight - 100) menu.className += " menu-color";
+                if (newlocal <= 400) {
+                    wrap.style.top = -newlocal / 5 + "px";
+                    footer.style.top = 150 - newlocal / 5 + "px";
+                } else if (wrap.style.top != "-80px" || footer.style.top != "70px") {
+                    wrap.style.top = "-80px";
+                    footer.style.top = "70px";
+                }
+            }
+            this.barlocal = newlocal;
         },
     },
 });
-App.mount("#layout");
+app.mount("#layout");
